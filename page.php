9afd2200173648af5cfbc5626def06673eb0525a
Maxime BERNARD-JACQUET <maxime@smoothie-creative.com>
2012-12-05 15:40:58 +0100

    resolve issue #2 by adding media queries mixin
    come in 3 mixins :
    
    desktop
    tablet
    smartphone
    
    Signed-off-by: Maxime BERNARD-JACQUET <maxime@smoothie-creative.com>

--- page.php
+++ page.php
@@ -1,11 +1,11 @@
 <?php
 	get_header();
-	if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+	if ( have_posts() ) : while ( have_posts() ) : the_post();
 ?>
 
 	<h1><?php the_title() ?></h1>
 
-	<div class="content row">
+	<div class="content">
 		<?php the_content() ?>
 	</div>
 
