d1e3bc7f6072225ff69ceb83d4d74ba1e2e6b52e
Maxime BERNARD-JACQUET <maxime@smoothie-creative.com>
2013-02-24 21:31:36 +0100

    Theme improvements
    - removed close tag php on functions to avoid header errors
    
    - added taxonomy.php which auto redirect taxonomies to archive-custom corresponding
    
    - improvement of taxo code in functions.php
    
    - improvement of media queries mixins
    
    Signed-off-by: Maxime BERNARD-JACQUET <maxime@smoothie-creative.com>

--- comments.php
+++ comments.php
@@ -1,40 +1,23 @@
 <?php
-if ( post_password_required() )
-	return;
+  // call me with comments_template();
+  if ( post_password_required() ) return;
 ?>
 
 <div id="comments" class="comments-area">
 
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'twentytwelve' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			?>
-		</h2>
+  <?php if ( have_comments() ) : ?>
+    <h2 class="comments-title"> Commentaires (<?php echo get_comments_number(); ?>)</h2>
 
-		<ol class="commentlist">
-			<?php wp_list_comments( array( 'callback' => 'twentytwelve_comment', 'style' => 'ol' ) ); ?>
-		</ol><!-- .commentlist -->
+    <ol class="commentlist">
+      <?php wp_list_comments(); ?>
+    </ol><!-- .commentlist -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below" class="navigation" role="navigation">
-			<h1 class="assistive-text section-heading"><?php _e( 'Comment navigation', 'twentytwelve' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentytwelve' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentytwelve' ) ); ?></div>
-		</nav>
-		<?php endif; // check for comment navigation ?>
+    <?php if ( ! comments_open() && get_comments_number() ) : ?>
+    <p class="nocomments">Les commentaires sont fermés sur cet article</p>
+    <?php endif; ?>
 
-		<?php
-		/* If there are no comments and comments are closed, let's leave a note.
-		 * But we only want the note on posts and pages that had comments in the first place.
-		 */
-		if ( ! comments_open() && get_comments_number() ) : ?>
-		<p class="nocomments"><?php _e( 'Comments are closed.' , 'twentytwelve' ); ?></p>
-		<?php endif; ?>
+  <?php endif; ?>
 
-	<?php endif; // have_comments() ?>
+  <?php comment_form(); ?>
 
-	<?php comment_form(); ?>
-
-</div><!-- #comments .comments-area -->
+</div> <!-- #comments .comments-area -->
---
+++ taxonomy.php
@@ -0,0 +1,21 @@
+<?php
+
+  // search for a archive-[custom-type].php instead of trying to load taxonomy-[taxo].php and if not found, goes to standard archive.php
+  // this code check if a archive-[custom-type].php exists where [custom-type] is the type which [taxo] belongs to.
+
+	// get queried object
+	$queried_object = $wp_query->get_queried_object();
+
+	// get actual taxonomy from object
+	$taxonomy = $queried_object->taxonomy;
+
+	// all taxonomy datas
+	$tax_object = get_taxonomy($taxonomy);
+
+	// among the datas, the custom type
+	$custom_type = $tax_object->object_type[0];
+
+  if (file_exists('archive-'.$custom_type.'.php')) {
+    get_template_part('archive-'.$custom_type);
+  }
+
