27d8a4906869d525e60190f1cd8783be1331fd51
Maxime BERNARD-JACQUET <maxime@smoothie-creative.com>
2014-12-19 14:53:03 +0100

    improved archive and single base markup

--- archive.php
+++ archive.php
@@ -1,49 +1,79 @@
 <?php
 	get_header();
-?>
-
-<?php
 
 	// case : display a category
 	if (is_category()) {
-		$title = "Catégorie : ".single_tag_title( '', false);
+		$title = "Catégorie &bullet; ".single_tag_title( '', false);
 	}
 
 	// case : display a tag
 	elseif (is_tag()) {
-		$title = "Mot clé : ".single_tag_title( '', false);
+		$title = "Mot clé &bullet; ".single_tag_title( '', false);
 	}
 
 	// case : display a tag
 	elseif (is_search()) {
-		$title = "Recherche : ".get_search_query();
+		$title = "Recherche &bullet; ".get_search_query();
 	}
 
 	else {
-		$title = "Actualités";
+		$title = 'Le Blog &bullet; dernières actus';
 	}
-
 ?>
+	
+	<div class="section blog">
+		<div class="wrapper flex-container">
+			<div class="f3 blog__posts">
+				<h1 class="blog__title"><?php echo $title; ?></h1>
 
-	<h1><?php echo $title; ?></h1>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				
+				<div class="post">
+					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-	<div class="blog">
+					<div class="post__meta">
+						<span class="post__date">Le <?php the_time('d F Y'); ?></span>
+						<span>&bullet;</span>
+						<span class="post__categories">Thème : <?php the_category(); ?></span> 
+						<span>&bullet;</span>
+						<span class="post__tags"> Tags : <?php the_tags('',' ',''); ?></span>
+					</div>
+					
+					<div class="post__thumbnail">
+						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+					</div>
+					
+					<div class="post__content">
+						<?php the_excerpt(); ?>
+						<p class="right"><a href="<?php the_permalink(); ?>" class="button--type1 button--small button--bold">Lire l'article</a></p>
+					</div>
+				</div>
 
-<?php
-	if ( have_posts() ) : while ( have_posts() ) : the_post();
+				<?php endwhile; endif; ?>
 
-		get_template_part('parts/article');
+				<div class="blog__navigation">
+				<?php 
 
-	endwhile;
-	endif;
-?>
+					global $wp_query;
 
+					$big = 999999999; // need an unlikely integer
+
+					echo paginate_links( array(
+						'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+						'format' => '?paged=%#%',
+						'current' => max( 1, get_query_var('paged') ),
+						'total' => $wp_query->max_num_pages
+					) );
+
+				?>
+				</div>
+			</div>
+
+			<div class="f1 blog__sidebar">
+				<?php dynamic_sidebar('Blog'); ?>
+			</div>
+		</div>
 	</div>
-
-	<div class="sidebar">
-		<?php //dynamic_sidebar('Blog'); ?>
-	</div>
-
 <?php
 	get_footer();
 ?>
