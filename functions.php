4babd432b610fa67c3c50bb3cd7232e18a16ef2b
Maxime BERNARD-JACQUET <maxime@smoothie-creative.com>
2015-02-24 11:03:35 +0100

    added ACF Options snippet

--- functions.php
+++ functions.php
@@ -101,6 +101,43 @@ add_action( 'init', 'smoothie_create_post_type' );
 */
 
 
+//  =====================
+//  = ACF Options pages =
+//  =====================
+
+/*
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page(array(
+		'page_title' 	=> 'SFly Options',
+		'menu_title'	=> 'SFly Options',
+		'menu_slug' 	=> 'sfly-options',
+		'capability'	=> 'edit_posts',
+		'position'		=> 3,
+		'icon_url'		=> 'dashicons-welcome-widgets-menus'
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Header',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'sfly-options',
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Blog',
+		'menu_title'	=> 'Blog',
+		'parent_slug'	=> 'sfly-options',
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Footer',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'sfly-options',
+	));
+}
+*/
+
+
 //  ======================================
 //  = Text domain for Multilingual theme =
 //  ======================================
