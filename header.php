bbfbac89c4b94893f60be0f6da41ed8ee707bca5
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2019-02-01 10:16:26 +0100

    fix header

--- header.php
+++ header.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/main.css">
-    <?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
   <div class="header">
     <div class="container">
-      <a href="<?php echo home_url('/'); ?>"></a> <?php _e('Hello there !', 'dysign'); ?>
-      <?php wp_nav_menu( array('theme_location' => 'main', 'container' => false, 'menu_class' => 'menu')); ?>
+      <a href="<?php echo home_url( '/' ); ?>"><?php _e('Hello there !', 'dysign'); ?></a> 
+      <?php wp_nav_menu( array( 'theme_location' => 'main', 'container' => false, 'menu_class' => 'menu') ); ?>
     </div>
   </div>
