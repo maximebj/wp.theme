ad2844a978aeb1d709b6ff45fe9aab1c4ad7b39d
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2019-02-01 10:16:35 +0100

    improve functions config

--- core/theme-config.php
+++ core/theme-config.php
@@ -8,12 +8,12 @@
 function dysign_theme_setup() {
 
   // Text Domain
-  load_theme_textdomain('dysign', get_template_directory() . '/languages');
+  load_theme_textdomain( 'dysign', get_template_directory() . '/languages' );
 
   //  Thumbnails
-  add_theme_support('post-thumbnails');
-  set_post_thumbnail_size(800, 600, false);
-  add_image_size('fullwidth', 1920, 0, false);
+  add_theme_support( 'post-thumbnails' );
+  set_post_thumbnail_size( 800, 600, false );
+  add_image_size( 'fullwidth', 1920, 0, false );
 
   //  Page Title
   add_theme_support( 'title-tag' );
@@ -22,9 +22,10 @@ function dysign_theme_setup() {
   register_nav_menus( array(
     'main' => 'Menu Principal',
     'footer' => 'Pied de page'
-  ));
+  ) );
 
-  // Editor Tiny MCE custom styles
+  // Editor custom styles
+  add_theme_support( 'editor-styles' );
   add_editor_style( array( 'css/editor-style.css') );
 
   // Enable HTML5
@@ -32,12 +33,16 @@ function dysign_theme_setup() {
     'search-form',
     'comment-form',
     'comment-list',
-    'gallery',
-    'caption',
-  ));
+  ) );
 
   // RSS
   add_theme_support( 'automatic-feed-links' );
+
+  // Remove post format
+  remove_theme_support( 'post-formats' );
+
+  // Gutenberg - Format large
+  add_theme_support( 'align-wide' );
 
 }
 add_action( 'after_setup_theme', 'dysign_theme_setup' );
@@ -47,17 +52,17 @@ add_action( 'after_setup_theme', 'dysign_theme_setup' );
 //  = Load scripts =
 //  ================
 
-function dysign_add_js_scripts() {
+function dysign_assets() {
 
   wp_deregister_script( 'jquery' );
-  wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', false, '3.2.1', true);
-  wp_register_script('theme-js', get_template_directory_uri().'/js/script.js', array('jquery'), '1.0', true );
+  
+  wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', false, '3.3.1', true);
+  wp_enqueue_script( 'dysign', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), '1.0', true );
 
-  wp_enqueue_script('jquery');
-  wp_enqueue_script('theme-js');
+  wp_enqueue_style( 'dysign', get_template_directory_uri() . '/css/main.css', array(), '1.0' );
 
 }
-add_action('wp_enqueue_scripts', 'dysign_add_js_scripts');
+add_action( 'wp_enqueue_scripts', 'dysign_assets' );
 
 
 
@@ -105,20 +110,6 @@ add_filter('excerpt_more', 'dysign_excerpt_more');
 // }
 // add_action('admin_enqueue_scripts', 'dysign_admin_theme_style');
 
-
-//  ===========================
-//  = Tiny MCE customization  =
-//  ===========================
-
-function dysign_custom_tinymce($init) {
-
-  $init['block_formats'] = 'Paragraphe=p;Titre 2=h2;Titre 3=h3;Titre 4=h4';
-  return $init;
-
-}
-add_filter('tiny_mce_before_init', 'dysign_custom_tinymce' );
-
-
 //  ===========================
 //  = Change author permalink =
 //  ===========================
@@ -127,7 +118,7 @@ function dysign_change_author_permalinks() {
     global $wp_rewrite;
     $wp_rewrite->author_base = 'auteur';
 }
-add_action('init','dysign_change_author_permalinks');
+add_action( 'init','dysign_change_author_permalinks' );
 
 
 // =====================
@@ -161,7 +152,7 @@ add_filter( 'sanitize_file_name', 'remove_accents' );
 //  ====================
 
 function dysign_remove_meta_boxes() {
-  remove_meta_box('dashboard_primary', 'dashboard', 'normal');
+  remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );
 }
 add_action( 'admin_menu', 'dysign_remove_meta_boxes' );
 
@@ -223,24 +214,21 @@ add_filter( 'upload_mimes', 'dysign_mime_types' );
 //  ============
 
 // Remove XML RPC
-add_filter('xmlrpc_enabled', '__return_false');
-
-// Gallery styles
-add_filter( 'use_default_gallery_style', '__return_false' );
+add_filter( 'xmlrpc_enabled', '__return_false' );
 
 // Welcome panel
-remove_action('welcome_panel', 'wp_welcome_panel');
+remove_action( 'welcome_panel', 'wp_welcome_panel' );
 
 // Head useless stuff
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wp_generator');
-remove_action('wp_head', 'feed_links', 2);
-remove_action('wp_head', 'index_rel_link');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'feed_links_extra', 3);
-remove_action('wp_head', 'start_post_rel_link', 10, 0);
-remove_action('wp_head', 'parent_post_rel_link', 10, 0);
-remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wp_generator' );
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'index_rel_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
 
 // Remove Emojis
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
@@ -250,15 +238,6 @@ remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-
-function dysign_disable_emojicons_tinymce( $plugins ) {
-  if ( is_array( $plugins ) ) {
-    return array_diff( $plugins, array( 'wpemoji' ) );
-  } else {
-    return array();
-  }
-}
-add_filter( 'tiny_mce_plugins', 'dysign_disable_emojicons_tinymce' );
 
 
 //  ===========================
@@ -280,7 +259,7 @@ function dysign_dashboard_widget_function( $post, $callback_args ) {
 function dysign_add_dashboard_widget() {
   wp_add_dashboard_widget('dysign_dashboard_widget', 'Dysign', 'dysign_dashboard_widget_function');
 }
-add_action('wp_dashboard_setup', 'dysign_add_dashboard_widget', 1 );
+add_action( 'wp_dashboard_setup', 'dysign_add_dashboard_widget', 1 );
 
 
 //  ========================
@@ -292,4 +271,4 @@ function dysign_change_footer() {
 
   echo $html;
 }
-add_filter('admin_footer_text', 'dysign_change_footer');
+add_filter( 'admin_footer_text', 'dysign_change_footer' );
